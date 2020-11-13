118ee72a5a4377cf20c0d08fc37fc195dff23a56
Adeniyi Olaitan <68019839+HectorGitt@users.noreply.github.com>
2020-11-13 16:17:12 +0100

    Update smtpgui.py
    help button Added, Send mail button added, with help window and quit comfirmation

--- smtpgui.py
+++ smtpgui.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter  import messagebox
+from tkinter.messagebox import askquestion
 from tkinter.filedialog import askopenfile
 import smtplib, time, os
 root = Tk()
@@ -9,8 +10,10 @@ smtpObj = ""
 server_frame = Frame(borderwidth= 5)
 login_frame = Frame(padx = 5, bg = "grey")
 file_frame = Frame()
+help_frame = Frame(borderwidth= 3)
 filenamepath ="None Selected......"
-
+HelpTxt ="Before You use this application, you would be required to upload a .vtx file format which will contain the first name The application requires you to select a server eg. if your email Address is example@gmail.com, then the server that should be selected will be the gmail server. After selecting the sever, the application requires to input the email and passwod of the operator(Check our privacy agreement on your account information), "
+Developer = "Batch mail was Developed by a Nigerian Software Developer named Adeniyi Olaitan Lekan As a si"
 Gmail = Radiobutton(server_frame, text="Gmail", variable = server, value="smtp.gmail.com", tristatevalue = "x", borderwidth = 2, relief = "groove" )
 Yahoo = Radiobutton(server_frame, text="Yahoo", variable = server, value="smtp.mail.yahoo.com", tristatevalue = "x", borderwidth = 2, relief = "groove" )
 Outlook = Radiobutton(server_frame, text="Outlook", variable = server, value="smtp-mail.outlook.com", tristatevalue = "x", borderwidth = 2, relief = "groove" )
@@ -22,6 +25,8 @@ def account():
         emailadd = email_box.get()
         passcode = password_box.get()
         smtpObj.login(emailadd, passcode)
+    else:
+        messagebox.showerror("Error", "Please input an email and password you moronic id*ot")
 
 def openfile():
     file = askopenfile(mode = "r", filetypes = [("Veritext file", "*.vtx")])
@@ -30,6 +35,9 @@ def openfile():
         content = file.read()
         filenamepath = file.name
         file_label.configure(text= filenamepath)
+    else:
+        pass
+
 
 def login():
     try:
@@ -49,8 +57,15 @@ def login():
             messagebox.showerror("Error", "Please select a server")
 
     except:
-        messagebox.showerror("Error", 'Check your internet connection \n If Problem persists, refer to the administrator')
+        messagebox.showerror("Error", 'Check your Internet connection \n If Problem persists, refer to the administrator')
 
+def sendmail():
+    pass
+
+def quit():
+    answer = askquestion(title='Quit?', message='Really quit?')
+    if answer=='yes':
+        root.destroy()
 
 #Define File button and labels
 fileopen = Button(file_frame, text="Load file", font=("Calibri", 12), command= openfile)
@@ -68,6 +83,21 @@ email_label= Label(login_frame, text="Enter Your Email: ", font=("Calibri", 12))
 
 #Defining login button
 login_but = Button(login_frame, text="Log In", font=("Calibri", 12), command= login)
+
+#Defining the Send message Button
+SendMsg = Button(text="Send Mail", font=("Calibri", 12))
+
+#New window
+new_window = Toplevel()
+new_window.title("Help Menu")
+label = Label(new_window, text=HelpTxt)
+label.grid(row=0, column=0)
+
+#Defining the Help button
+help_but = Button(help_frame, text="Help", font=("Calibri", 12),command=new_window)
+
+
+
 
 #Grid Server name in Frame
 Gmail.grid(row= 0, column= 0, padx = 20)
@@ -87,19 +117,29 @@ password_label.grid(row= 0, column= 2)
 password_box.grid(row= 0, column= 3)
 login_but.grid(row=0, column = 4)
 
-#Grid login_frame
+#login_frame Gridding
 login_frame.grid(row=1, column= 0)
 
-#Grid file frame 
+#file frame  Gridding
 file_frame.grid(row=2, column = 0)
 
-#Grid file buttons and labels
+#file buttons and labels Gridding
 fileopen.grid(row=0, column = 0)
 file_label.grid(row=0, column= 1)
 
-#Grid email buttons
+#email buttons Gridding
 mail_box.grid(row=3, column = 0)
 
+#SendMsg to Window Gridding
+SendMsg.grid(row=4, column=0)
+
+#Help Frame Gridding
+help_frame.grid(row=5, column=0)
+
+#Help button Gridding
+help_but.grid(row=0, column=0, columnspan=5)
+
+root.protocol('WM_DELETE_WINDOW', quit)
 root.title("Batch-mail")
 root.geometry("700x600")
 mainloop()
